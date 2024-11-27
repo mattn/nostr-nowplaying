40c42a3e9e2112690ba87ddb6863b264be263a18
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2024-11-27 22:18:48 +0900

    add mpd

---
+++ nostr-nowplaying-mpd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+MPD_HOST="${MPD_HOST:-127.0.0.1}"
+
+while true; do
+  title=$(mpc -h $MPD_HOST current --wait)
+  if [ "$title" == "" ]; then
+    continue
+  fi
+  title_enc=$(echo $title | jq -Rr '@uri')
+  expiration=$(date -d'+5minutes' +%s)
+  algia event --kind 30315 --content "♫ $title" --tag d=music --tag expiration="$expiration" --tag r="spotify:search:$title_enc"
+done
